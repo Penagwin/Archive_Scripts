e1e3759b9fb767f30eb177773aac76fbfcd087f9
Penagwin <langpenguin@gmail.com>
2021-08-23 20:03:41 -0400

    Removed unneeded parts from the url command gen

--- generate_urls.py
+++ generate_urls.py
@@ -1,43 +1,13 @@
 
 import argparse
-import os
 import random
-
-import googleapiclient.discovery
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
-youtube = googleapiclient.discovery.build("youtube", "v3", developerKey = os.environ.get("YOUTUBE_API_KEY"))
 
-request = channels_response = youtube.channels().list(
-        forUsername="blackhatofficialyt",
-        part="id, snippet, statistics, contentDetails, topicDetails"
-).execute()
-
-print(request)
-request = youtube.playlistItems().list(
-    part = "snippet",
-    playlistId = "UUJ6q9Ie29ajGqKApbLqfBOg",#request['items'][0]['id'],
-    maxResults = 50
-)
-
-playlists = []
-
-
-while request is not None:
-    response = request.execute()
-    playlists += response["items"]
-    request = youtube.playlists().list_next(request, response)
-
-# Get each video from the playlist
-for video in playlists:
-	print(video['snippet']['resourceId']['videoId'])
-print(f"total: {len(playlists)}")
-#print(playlists)
-print(len(playlists))
 
 # URL for Mullvad's json API
 MULLVAD_SERVER_API_URL = 'https://api.mullvad.net/www/relays/all/'
@@ -67,7 +37,6 @@ CHANNELS = [
 # Grab the available SOCKS5 proxies from Mullvad
 resp = requests.get(url=MULLVAD_SERVER_API_URL)
 data = resp.json() 
-
 # Go through each of the servers and filter out the ones we need
 for server in data:
     if server.get("country_code") in SOCKS_COUNTRY_WHITELIST and server.get("city_code") not in SOCKS_CITY_BLACKLIST:
@@ -97,4 +66,4 @@ if selected_channel not in CHANNELS:
 for selected_channel in CHANNELS:
     selected_socks_proxy = random.choice(SOCKS5_SERVER_LIST)
 
-    print(f'cd {STORAGE_DIR}{selected_channel} && youtube-dl --get-id "https://www.youtube.com/{selected_channel}/videos" | xargs -I "{{}}" -P {parallel_threads} youtube-dl --proxy {selected_socks_proxy} -f bestvideo+bestaudio --write-description --write-info "{{}}" ')
+    print(f'cd {STORAGE_DIR}{selected_channel} && yt-dlp --get-id "https://www.youtube.com/{selected_channel}/videos" | xargs -I "{{}}" -P {parallel_threads} youtube-dl --proxy {selected_socks_proxy} -f bestvideo+bestaudio --write-description --write-info "{{}}" ')
